1b85c84539eeb561e2ef7b30297dcff5cc749ad2
ars1an <arslanmansoor22@gmail.com>
2022-05-09 17:18:48 +0530

    added functionality for rainbow and color mode, fixed dynamic grid sizing

--- sketch.js
+++ sketch.js
@@ -1,44 +1,85 @@
+var colorpick;
+var gridsize;
+
 function createGrid(n) { //here n is the grid size
-    console.log(n);
     const grid = document.querySelector('.grid');
     grid.innerHTML = ''; //prevents extra boxes from being added when function is called again
     for (let i = 0; i < (n*n); i++) {
         const div = document.createElement('div');
-        div.classList.toggle('boxdiv')
+        div.classList.toggle('boxdiv');
+        div.style.width = `calc(100% / ${n})`;
+        div.style.height = `calc(100% / ${n})`;
         grid.appendChild(div);
     }
     gridHover();
 }
 
-function gridColor(e) {
+function gridColor() {
+    if (colorpick === 'black') {
     this.style.backgroundColor = 'black';
+    } else if (colorpick === 'white') {
+        this.style.backgroundColor = 'white';
+    } else if (colorpick === 'rainbow') {
+        let newcolor = Math.floor(Math.random()*16777215).toString(16);
+        this.style.backgroundColor = '#' + newcolor;
+    }
 }
 
 function gridHover() {
     const gdiv = document.querySelectorAll('.boxdiv');
     const gdivlist = Array.from(gdiv); 
-    console.log('hi outside');
     for (let i = 0; i < gdivlist.length; i++) {
-        console.log('hi inside');
         gdivlist[i].addEventListener('mouseover', gridColor);
     }
 }
 
 function gridInitialize() {
-    createGrid(16);
+    if (gridsize === undefined || gridsize === 0) {
+        gridsize = 16;
+    }
+    colorpick = 'black';
+    createGrid(gridsize);
 }
 
 function userInput() {
-    let user = prompt("Enter the size of the grid [Max Size = 100]");
-    if (user !== null && user <= 100) {
-        createGrid(user);
+    let user = Number(prompt("Enter the size of the grid [Max Size = 100]"));
+    if (user > 100) {
+        while (user > 100) {
+            user = Number(window.prompt('Seriously, enter less than 100 * 100'));
+        }
+        gridsize = user;
+        gridInitialize();
+    } else if (user !== null && user > 0 && user <= 100) {
+        grildsize = user;
+        gridlInitialize();
     }
 }
 
-window.onload = createGrid(16);
+window.onload = gridInitialize;
 
 const input = document.getElementById('grid-size'); //grid size button
 input.addEventListener('click', userInput);
 
+const color = document.getElementById('color');
+color.addEventListener('click', function() {
+    colorpick = 'black';
+    gridHover();
+});
+
+const randomcolor = document.getElementById('rainbow');
+randomcolor.addEventListener('click', function() {
+    colorpick = 'rainbow';
+    gridHover();
+});
+
+const eraser = document.getElementById('eraser');
+eraser.addEventListener('click', function() {
+    colorpick = 'white';
+    gridHover();
+});
+
 const reset = document.getElementById('reset');
-reset.addEventListener('click', gridInitialize);
+reset.addEventListener('click', function() {
+    gridsize = 16;
+    gridInitialize();
+});
