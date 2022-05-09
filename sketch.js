ad72d1459f49cd93b39420f448ceec08ec2df53b
ars1an <arslanmansoor22@gmail.com>
2022-05-09 12:07:25 +0530

    added buttons and added prompt for user input on the grid size

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
-const grid = document.querySelector('.grid');
+function gridInitialize() {
+
+}
 
 function createGrid(n) { //here n is the grid size
+    const grid = document.querySelector('.grid');
+    grid.innerHTML = ''; //prevents extra boxes from being added when function is called again
     for (let i = 0; i < (n*n); i++) {
         const div = document.createElement('div');
         div.classList.toggle('boxdiv')
@@ -8,18 +12,28 @@ function createGrid(n) { //here n is the grid size
     }
 }
 
-function gridInitialize() {
-
-}
-
 function gridColor(e) {
     this.style.backgroundColor = 'black';
+}
+
+function userInput() {
+    console.log('hi');
+    let user = prompt("Enter the size of the grid [Max Size = 100]");
+    if (user !== null && user <= 100) {
+        console.log(user);
+        createGrid(user);
+    }
 }
 
 window.onload = createGrid(16);
 
 const gdiv = document.querySelectorAll('.boxdiv');
 const gdivlist = Array.from(gdiv); 
+console.log('hi outside');
 for (let i = 0; i < (16*16); i++) {
+    console.log('hi inside');
     gdivlist[i].addEventListener('mouseover', gridColor);
 }
+
+const input = document.getElementById('grid-size'); //grid size button
+input.addEventListener('click', userInput);
