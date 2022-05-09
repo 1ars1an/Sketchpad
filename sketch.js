2e0830e21452e897263d58f4c1aff6b378b55144
ars1an <arslanmansoor22@gmail.com>
2022-05-09 14:33:28 +0530

    added reset functionality and abstracted the hover functionality

--- sketch.js
+++ sketch.js
@@ -1,8 +1,5 @@
-function gridInitialize() {
-
-}
-
 function createGrid(n) { //here n is the grid size
+    console.log(n);
     const grid = document.querySelector('.grid');
     grid.innerHTML = ''; //prevents extra boxes from being added when function is called again
     for (let i = 0; i < (n*n); i++) {
@@ -10,30 +7,38 @@ function createGrid(n) { //here n is the grid size
         div.classList.toggle('boxdiv')
         grid.appendChild(div);
     }
+    gridHover();
 }
 
 function gridColor(e) {
     this.style.backgroundColor = 'black';
 }
 
+function gridHover() {
+    const gdiv = document.querySelectorAll('.boxdiv');
+    const gdivlist = Array.from(gdiv); 
+    console.log('hi outside');
+    for (let i = 0; i < gdivlist.length; i++) {
+        console.log('hi inside');
+        gdivlist[i].addEventListener('mouseover', gridColor);
+    }
+}
+
+function gridInitialize() {
+    createGrid(16);
+}
+
 function userInput() {
-    console.log('hi');
     let user = prompt("Enter the size of the grid [Max Size = 100]");
     if (user !== null && user <= 100) {
-        console.log(user);
         createGrid(user);
     }
 }
 
 window.onload = createGrid(16);
 
-const gdiv = document.querySelectorAll('.boxdiv');
-const gdivlist = Array.from(gdiv); 
-console.log('hi outside');
-for (let i = 0; i < (16*16); i++) {
-    console.log('hi inside');
-    gdivlist[i].addEventListener('mouseover', gridColor);
-}
-
 const input = document.getElementById('grid-size'); //grid size button
 input.addEventListener('click', userInput);
+
+const reset = document.getElementById('reset');
+reset.addEventListener('click', gridInitialize);
